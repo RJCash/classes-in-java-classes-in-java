96ef348f345c29d541a9c5e328cfe30c4e241d7e
Rickie Cashwell <rjc6056@gmail.com>
2017-03-03 11:53:21 -0500

    Classes example changes

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -2,3 +2,9 @@
  * Create the most minimal class you can. The class must be named "Animal"
  * because it must match the file name.
  */
+public class Animal{
+    public static void main(String[] args) {
+        System.out.println("Im animal");
+    }
+
+}
